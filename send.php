8aa420320e0799d78a5d799e59285f2c810343e9
sciron <kozin.alex@gmail.com>
2016-10-31 13:59:50 +0300

    send.php

---
+++ send.php
@@ -0,0 +1,11 @@
+$Name = $_POST['Name'];
+$Phone = $_POST['Phone'];
+$modalFrom = $_POST['modalFrom'];
+$modalTo = $_POST['modalTo'];
+$date = $_POST['Date'];
+$Time = $_POST['Time'];
+//echo $Name;
+//echo "<br>";
+//echo $Phone;
+} else { 
+}?>
